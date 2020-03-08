f2bee106667b1a6794cfe2f6a875c923f8b6a4f2
Alessandro Provenza <ap.developer@outlook.com>
2020-03-08 04:00:58 +0100

    added history search

---
+++ consortium-network/ledger/web3js-eea/clb-smartContracts/solidity/RFIDTags/RfidTagsCollection.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.5.0;
+
+contract RfidTagsCollection {
+  uint public count = 0;
+  address private contractOwner;
+
+  constructor() public { contractOwner = msg.sender; }
+
+  modifier onlyContractOwner() {
+    require(
+      msg.sender == contractOwner,
+      "Only the owner of this contract (RFID supplier) can generate new UUIDs."
+    );
+    _;
+  }
+
+  modifier onlyBrand(address _brand) {
+    require(
+      msg.sender == _brand,
+      "A brand owner can mark as invalid only its own UUIDs."
+    );
+    _;
+  }
+
+  struct RFT {
+    string uuid;
+    uint exchanges;
+    address brand;
+    address[] owners;
+    bool isValid;
+  } mapping(string => RFT) private rfts;
+
+  function create(string memory _uuid, address _brand)
+  public onlyContractOwner {
+    count++;
+    RFT memory _rft;
+    address[] memory _owners = new address[](2);
+    _owners[0] = contractOwner;
+    _owners[1] = _brand;
+    _rft.uuid = _uuid;
+    _rft.exchanges = 0;
+    _rft.brand = _brand;
+    _rft.owners = _owners;
+    _rft.isValid = true;
+    rfts[_uuid] = _rft;
+    emit GenerateNewTag(contractOwner, _uuid, _brand, true);
+  }
+
+  function invalidate(string memory _uuid, address _brand)
+  public onlyBrand(_brand) {
+    RFT memory _rft = rfts[_uuid];
+    _rft.isValid = !_rft.isValid;
+    rfts[_uuid] = _rft;
+    emit MarkTagInvalid(_uuid, _rft.isValid);
+  }
+  
+  function getOwners (string memory _uuid) 
+  public {
+    for(uint i=0; i<rfts[_uuid].owners.length;i++)
+      emit LogOwnersHistory(rfts[_uuid].uuid, rfts[_uuid].owners[i]);
+  }
+
+  event GenerateNewTag(address contractOwner, string uuid, address brand, bool isValid);
+  event MarkTagInvalid(string uuid, bool isValid);
+  event LogOwnersHistory(string uuid, address owner);
+  
+}
