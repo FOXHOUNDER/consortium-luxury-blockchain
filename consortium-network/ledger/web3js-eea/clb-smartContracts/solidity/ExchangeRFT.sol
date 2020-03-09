ec6b6cc6819462af4ad3e94c80c5e3b30b4228ef
Alessandro Provenza <ap.developer@outlook.com>
2020-03-09 04:48:44 +0100

    exchange contract v1

--- consortium-network/ledger/web3js-eea/clb-smartContracts/solidity/ExchangeRFT.sol
+++ consortium-network/ledger/web3js-eea/clb-smartContracts/solidity/ExchangeRFT.sol
@@ -1,16 +1,42 @@
+pragma experimental ABIEncoderV2;
 pragma solidity ^0.5.0;
 
 import "./SharedObjectsLibrary.sol";
+import "./RFTagsCollection.sol";
+import "./TrustScoresCollection.sol";
 
 contract ExchangeRFT {
   uint public exCount = 0;
+  address rft_address = 0x804F1021aBecc8453Ea6B13045998799503aD5d3;
+  address ts_address = 0xC74C15333F64F2B0235Fe580206DD7e579a6cE56;
+  RFTagsCollection rftcontract = RFTagsCollection(rft_address);
+  TrustScoresCollection tsContract = TrustScoresCollection(ts_address);
   
   struct Exchange {
     uint transferId;
     address from;
     address to;
-    SharedObjectsLibrary.RFT[] set;
-    bool isValid;
-  } mapping(string => Exchange) private exchanges;
+    string[] set;
+    string status;
+  } mapping(uint => Exchange) public exchanges;
+ 
+  function beginTransfer(address _from, address _to, string[] memory _set)
+  public returns (uint transferId) {
+    exCount++;
+    Exchange memory _exchanges;
+    if(tsContract.getCurrentScore(_to) > 70) {
+      for(uint i=0;i<_set.length;i++)
+        if(msg.sender==rftcontract.getCurrentOwner(_set[i])) {
+          _exchanges.transferId = exCount;
+          _exchanges.to = _to;
+          _exchanges.set = _set;
+          _exchanges.status = "SENT";
+          exchanges[exCount] = _exchanges;
+        }
+    }
+    return exCount;
+  }
+  
+
   
 }
